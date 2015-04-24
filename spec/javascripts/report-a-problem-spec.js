814b8242ce333c80a75726e2348829c1710e6e36
Paul Hayes <fofr@users.noreply.github.com>
2015-04-24 11:40:40 +0100

    Whitespace: 4 spaces to 2 spaces
    File was using 4 spaces for tabs instead of 2.

--- spec/javascripts/report-a-problem-spec.js
+++ spec/javascripts/report-a-problem-spec.js
@@ -1,104 +1,99 @@
 describe("form submission for reporting a problem", function () {
-    var $form, reportAProblem;
+  var $form, reportAProblem;
+
+  beforeEach(function() {
+    setFixtures('\
+              <div class="report-a-problem-container">\
+                <div class="report-a-problem-content">\
+                  <form><button class="button" name="button" type="submit">Send</button></form>\
+                </div>\
+              </div>');
+    $form = $('form');
+  });
+
+  describe("when not included in redesign test", function() {
 
     beforeEach(function() {
-        setFixtures('<div class="report-a-problem-container">\
-                          <div class="report-a-problem-content">\
-                              <form><button class="button" name="button" type="submit">Send</button></form>\
-                          </div>\
-                     </div>');
-        $form = $('form');
+      spyOn(GOVUK.ReportAProblem, 'isBeingTestedOnThisPage').and.returnValue(false);
+      spyOn(GOVUK, 'MultivariateTest');
+      reportAProblem = new GOVUK.ReportAProblem($('.report-a-problem-container'));
     });
 
-    describe("when not included in redesign test", function() {
-
-        beforeEach(function() {
-            spyOn(GOVUK.ReportAProblem, 'isBeingTestedOnThisPage').and.returnValue(false);
-            spyOn(GOVUK, 'MultivariateTest');
-            reportAProblem = new GOVUK.ReportAProblem($('.report-a-problem-container'));
-        });
-
-        it("does not create a multivariate test", function(){
-            expect(GOVUK.MultivariateTest).not.toHaveBeenCalled();
-            expect(reportAProblem.multivariateTest).toBe(undefined);
-        });
-
-        testBehaviour();
+    it("does not create a multivariate test", function(){
+      expect(GOVUK.MultivariateTest).not.toHaveBeenCalled();
+      expect(reportAProblem.multivariateTest).toBe(undefined);
     });
 
-    describe("when included in redesign test", function() {
+    testBehaviour();
+  });
 
-        beforeEach(function() {
-            spyOn(GOVUK.ReportAProblem, 'isBeingTestedOnThisPage').and.returnValue(true);
-        });
+  describe("when included in redesign test", function() {
 
-        it('creates a multivariate test', function() {
-            reportAProblem = new GOVUK.ReportAProblem($('.report-a-problem-container'));
-            expect(reportAProblem.multivariateTest.name).toBe('report-a-problem-redesign-ab-test');
-        });
-
-        describe("when showing original variant", function(){
-            beforeEach(function() {
-                spyOn(GOVUK, 'cookie').and.returnValue('variant_0');
-                reportAProblem = new GOVUK.ReportAProblem($('.report-a-problem-container'));
-            });
-
-            it('uses variant_0', function() {
-                expect(reportAProblem.multivariateTest.getCohort()).toBe('variant_0');
-            });
-
-            testBehaviour();
-        });
-
-        describe("when showing new variant", function(){
-            beforeEach(function() {
-                spyOn(GOVUK, 'cookie').and.returnValue('variant_1');
-                reportAProblem = new GOVUK.ReportAProblem($('.report-a-problem-container'));
-            });
-
-            it('uses variant_1', function() {
-                expect(reportAProblem.multivariateTest.getCohort()).toBe('variant_1');
-            });
-
-            testBehaviour();
-        });
+    beforeEach(function() {
+      spyOn(GOVUK.ReportAProblem, 'isBeingTestedOnThisPage').and.returnValue(true);
     });
 
+    it('creates a multivariate test', function() {
+      reportAProblem = new GOVUK.ReportAProblem($('.report-a-problem-container'));
+      expect(reportAProblem.multivariateTest.name).toBe('report-a-problem-redesign-ab-test');
+    });
 
-    function testBehaviour() {
+    describe("when showing original variant", function(){
+      beforeEach(function() {
+        spyOn(GOVUK, 'cookie').and.returnValue('variant_0');
+        reportAProblem = new GOVUK.ReportAProblem($('.report-a-problem-container'));
+      });
 
-        describe("clicking on the toggle", function(){
-            it("should toggle the visibility of the form", function() {
-                expect($form).toBeVisible();
+      it('uses variant_0', function() {
+        expect(reportAProblem.multivariateTest.getCohort()).toBe('variant_0');
+      });
 
-                $('a').click();
+      testBehaviour();
+    });
 
-                expect($form).toBeHidden();
+    describe("when showing new variant", function(){
+      beforeEach(function() {
+        spyOn(GOVUK, 'cookie').and.returnValue('variant_1');
+        reportAProblem = new GOVUK.ReportAProblem($('.report-a-problem-container'));
+      });
 
-                $('a').click();
+      it('uses variant_1', function() {
+        expect(reportAProblem.multivariateTest.getCohort()).toBe('variant_1');
+      });
 
-                expect($form).toBeVisible();
-            });
-        });
+      testBehaviour();
+    });
+  });
 
-        describe("if the request succeeds", function() {
-            it("should replace the form with the response from the AJAX call", function() {
+  function testBehaviour() {
+    describe("clicking on the toggle", function(){
+      it("should toggle the visibility of the form", function() {
+        expect($form).toBeVisible();
+        $('a').click();
+        expect($form).toBeHidden();
+        $('a').click();
+        expect($form).toBeVisible();
+      });
+    });
 
-                $form.trigger('reportAProblemForm.success', {message: 'great success!'});
+    describe("if the request succeeds", function() {
+      it("should replace the form with the response from the AJAX call", function() {
 
-                expect($form).toBeHidden();
-                expect($('.report-a-problem-content').html()).toEqual('great success!');
-            });
-        });
+        $form.trigger('reportAProblemForm.success', {message: 'great success!'});
 
-        describe("if the request has failed", function() {
-            it("should display an error message", function() {
+        expect($form).toBeHidden();
+        expect($('.report-a-problem-content').html()).toEqual('great success!');
+      });
+    });
 
-                $form.trigger('reportAProblemForm.error');
+    describe("if the request has failed", function() {
+      it("should display an error message", function() {
 
-                expect($form).not.toBeVisible();
-                expect($('.report-a-problem-content').html()).toContain("Sorry, we’re unable to receive your message");
-            });
-        });
-    }
+        $form.trigger('reportAProblemForm.error');
+
+        expect($form).not.toBeVisible();
+        expect($('.report-a-problem-content').html()).toContain("Sorry, we’re unable to receive your message");
+      });
+    });
+  }
 });
